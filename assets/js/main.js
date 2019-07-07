08122d6c05f3670a55376203b85798c1eacbf67f
Jonathan Giardino <j.giardino@me.com>
2019-07-07 14:56:56 +0100

    content check

--- assets/js/main.js
+++ assets/js/main.js
@@ -242,7 +242,7 @@ function smoothScroll(eID) {
   }
   var speed = Math.round(distance / 100);
   if (speed >= 20) speed = 20;
-  var step = Math.round(distance / 30);
+  var step = Math.round(distance / 40);
   var leapY = stopY > startY ? startY + step : startY - step;
   var timer = 0;
   if (stopY > startY) {
